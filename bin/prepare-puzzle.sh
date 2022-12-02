0f28072c38e3843a143f87c7d0c6679206a92189
Noah Callaway <ncallaway@gmail.com>
2022-12-01 22:50:42 -0800

    Add prepare puzzle script

---
+++ bin/prepare-puzzle.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+if [[ -z $1 ]] ; then echo 'no day given'; exit 1; fi
+if [[ -z $2 ]] ; then echo 'no puzzle given'; exit 1; fi
+
+APP_ROOT="$(dirname "$(dirname "$(readlink -fm "$0")")")"
+
+DAY="day-$1"
+PUZZLE="puzzle-$2"
+
+cd $APP_ROOT
+
+mkdir -p $DAY
+cd $DAY
+
+if [[ -d "$PUZZLE" ]] ; then echo "puzzle $DAY/$PUZZLE already exists"; exit 1; fi
+
+echo "preparing puzzle for $DAY, $PUZZLE"
+cargo new $PUZZLE
+
